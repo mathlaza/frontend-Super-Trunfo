a83c26a5271eb0ec2467599299b93e948b9694d5
Matheus Martinez <decretomusic@gmail.com>
2023-02-26 21:05:26 -0300

    feat: Card component and Passing props from App to Card and Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Form />
+        <Form
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo={ false }
+          // hasTrunfo=""
+          isSaveButtonDisabled={ false }
+          onInputChange={ () => {} }
+          onSaveButtonClick={ () => {} }
+        />
+
+        <Card
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo={ false }
+        />
       </div>
     );
   }
